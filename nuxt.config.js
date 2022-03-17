bea2c72d4b16f07e238f71d9d8ba1755ea8ca526
Jamie Carter <9541902+jamiecarter7@users.noreply.github.com>
2022-03-17 13:21:48 +0000

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,6 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
-
-  mode: 'universal',
+  // ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
@@ -56,6 +54,7 @@ export default {
   },
 
   generate: {
-    fallback: true
+    modern: true,
+    fallback: true,
   }
 }
